9fd2a7b365bb420dbfb27ffa05bc80ceebf157e2
1 <2>
2018-02-18 08:44:05 +0100

    ListeChambre (toutes) Order By Chemin relatif Telechargement

--- GrandHotel/Controllers/HomeController.cs
+++ GrandHotel/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace GrandHotel.Controllers
         public FileResult Download()
         {
             string fileName = "Appli_Cliente.exe";
-            byte[] fileBytes = System.IO.File.ReadAllBytes($"S:/Maurice/ProjetASP/ProjetFinal/GrandHotel/wwwroot/Appli_Cliente/GH_API_Client.exe");
+            byte[] fileBytes = System.IO.File.ReadAllBytes($"wwwroot/Appli_Cliente/GH_API_Client.exe");
             return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
         }
 
